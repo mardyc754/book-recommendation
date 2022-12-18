4cbd6ab6f538c5a680e6428cccc53102244b2b6b
mardyc754 <mar754dyc@gmail.com>
2022-12-18 13:15:39 +0100

    Init frontend in next.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,3 @@ import App from './App';
 const app = new App();
 
 app.listen();
-
-// import express from 'express';
-// import 'dotenv/config';
-
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-// app.listen(port, () => {
-//   console.log(`Server listening on port: ${port}`);
-// });
